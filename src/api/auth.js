2e3a3b0edf6bcf525cc9b8a72599cbc74bd86175
helpMeRise <rises128@gmail.com>
2022-06-24 14:58:52 +0300

    api config added

---
+++ src/api/auth.js
@@ -0,0 +1,16 @@
+import {
+  URL_AUTH,
+  ACCESS_KEY,
+  RESPONSE_TYPE,
+  REDIRECT_URI,
+  SCOPE,
+} from './const';
+
+const searchParams = new URLSearchParams('');
+
+searchParams.append('client_id', ACCESS_KEY);
+searchParams.append('redirect_uri', REDIRECT_URI);
+searchParams.append('response_type', RESPONSE_TYPE);
+searchParams.append('scope', SCOPE);
+
+export const urlAuth = `${URL_AUTH}${searchParams.toString()}`;
---
+++ src/api/const.js
@@ -0,0 +1,5 @@
+export const URL_AUTH = 'https://unsplash.com/oauth/authorize';
+export const ACCESS_KEY = 'UqB4_QScuKWMydMf16Lj3hEDtws0t90nk_g36AgvIx8';
+export const RESPONSE_TYPE = 'code';
+export const REDIRECT_URI = 'http://192.168.1.2:3000';
+export const SCOPE = 'public read_user read_photos write_likes';
