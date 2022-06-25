5c2181577da5acb21156553caeae7718caf94797
helpMeRise <rises128@gmail.com>
2022-06-26 01:57:36 +0300

    Realize POST request for access token

--- src/components/Header/Auth/Auth.jsx
+++ src/components/Header/Auth/Auth.jsx
@@ -3,11 +3,11 @@ import style from './Auth.module.css';
 import { ReactComponent as LoginIcon } from './image/login.svg';
 import { urlAuth } from '../../../api/auth';
 import { PropTypes } from 'prop-types';
-// import { useAuth } from '../../../hooks/useAuth';
+import { useAuth } from '../../../hooks/useAuth';
 
 export const Auth = () => {
-  // const [auth] = useAuth();
-  console.log();
+  const [auth] = useAuth();
+  console.log(auth);
   return (
     <a href={urlAuth} className={style.button}>
       <LoginIcon className={style.icon}/>
